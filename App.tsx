bde949d660e573505d29274f9e29624cbeb78375
krishna <ramamurthikrishna7@email.com>
2026-02-13 13:40:30 +0530

    Revert logo usage to SVG due to download failure

--- App.tsx
+++ App.tsx
@@ -547,7 +547,7 @@ const App: React.FC = () => {
           {/* Emblem / Logo */}
           <div className="w-24 h-24 mb-6">
             <img
-              src="/assets/logo.png"
+              src="/assets/logo.svg"
               alt="Davanagere Municipal Corporation Emblem"
               className="w-full h-full object-contain opacity-80"
               onError={(e) => { (e.target as HTMLImageElement).style.display = 'none'; }}
